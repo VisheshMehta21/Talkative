04020fc5d80d2d0f2cb27f50045f4bad6ecc3621
VisheshMehta21 <visheshmehta21@gmail.com>
2025-01-18 20:09:35 -0800

    Added Login Redux.

--- Frontend/src/Redux/Auth/Action.js
+++ Frontend/src/Redux/Auth/Action.js
@@ -1,7 +1,8 @@
 import { BASE_API_URL } from '../../Config/Config.js';
 
 import {
-    SignUp
+    SignUp,
+    SignIn
   } from './ActionType';
 
   export const signup = (data) => async (dispatch) => {
@@ -19,5 +20,24 @@ import {
       dispatch({ type: SignUp, payload: resData });
     } catch (error) {
       console.error('Registration error:', error);
+    }
+  };
+
+
+  export const signin = (data) => async (dispatch) => {
+    try {
+      const res = await fetch(`${BASE_API_URL}/auth/signin`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      const resData = await res.json();
+      if (resData.jwt) localStorage.setItem('token', resData.jwt);
+      dispatch({ type: LOGIN_USER, payload: resData });
+    } catch (error) {
+      console.error('Login error:', error);
     }
   };
