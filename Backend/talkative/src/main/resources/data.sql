d708cd5b03aa42e8b8ef8af33ed0d8a9b6ab1376
VisheshMehta21 <visheshmehta21@gmail.com>
2025-02-02 22:02:22 -0800

    Docker.

--- Backend/talkative/src/main/resources/data.sql
+++ Backend/talkative/src/main/resources/data.sql
@@ -1,13 +1,1 @@
 CREATE DATABASE IF NOT EXISTS talkative;
-
-use talkative;
-
--- Inserting six users
-INSERT INTO users (first_name, last_name, email, password, is_verified, created_at)
-VALUES
-    ('Rachel', 'Green', 'rachel.green@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW()),
-    ('Monica', 'Geller', 'monica.geller@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW()),
-    ('Phoebe', 'Buffay', 'phoebe.buffay@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW()),
-    ('Chandler', 'Bing', 'chandler.bing@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW()),
-    ('Joey', 'Tribbiani', 'joey.tribbiani@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW()),
-    ('Ross', 'Geller', 'ross.geller@friends.com', '$2a$12$xqpn0xAce6dMcYM3vrh.QODeKzC5t4P5Z09hE1Nz2nerkIl00quka', true, NOW());
